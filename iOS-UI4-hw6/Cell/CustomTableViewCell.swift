d78ebbbac7883d46d8185f4cb56e497c2463f3e7
d1onnisss <lim_al@auca.kg>
2024-05-27 02:08:27 +0600

    CustomTableView implementation

---
+++ iOS-UI4-hw6/Cell/CustomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTableViewCell.swift
+//  iOS-UI4-hw6
+//
+//  Created by Alexey Lim on 27/5/24.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ iOS-UI4-hw6/Model/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  iOS-UI4-hw6
+//
+//  Created by Alexey Lim on 27/5/24.
+//
+
+import Foundation
